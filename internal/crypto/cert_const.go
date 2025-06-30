ab3a6e483d66e8dcf2983e89af760ab34226e185
Yujie Wu <coffee377@dingtalk.com>
2025-06-30 17:36:46 +0800

    feat: enhance cert management with structured types and paths

--- internal/crypto/cert_const.go
+++ internal/crypto/cert_const.go
@@ -1,16 +1,75 @@
 package crypto
 
+import (
+	"path/filepath"
+)
+
 const (
 	// CertBaseDir is the directory where all certificates are stored.
 	CertBaseDir = "certs"
 
-	CertEnvoyDir          = "envoy"
-	CertEnvoyGatewayDir   = "envoy-gateway"
-	CertEnvoyRateLimitDir = "envoy-rate-limit"
-	CertEnvoyOidcHmacDir  = "envoy-oidc-hmac"
+	CertEnvoy CertType = iota
+	CertEnvoyGateway
+	CertEnvoyRateLimit
+	CertEnvoyOidcHmac
 
-	CACertificateFile = "ca.crt"
-	CertificateFile   = "tls.crt"
-	PrivateKeyFile    = "tls.key"
-	HmacSecretFile    = "hmac-secret"
+	CACertificateFilename = "ca.crt"
+	CertificateFilename   = "tls.crt"
+	PrivateKeyFilename    = "tls.key"
+
+	HmacSecretFilename = "hmac-secret"
 )
+
+type CertType int
+
+func (c CertType) DirPath(baseDir ...string) string {
+	return filepath.Join(baseDir...)
+}
+
+func (c CertType) FilePath(filename string, baseDir ...string) string {
+	return filepath.Join(append(baseDir, c.String(), filename)...)
+}
+
+func (c CertType) String() string {
+	switch c {
+	case CertEnvoy:
+		return "envoy"
+	case CertEnvoyGateway:
+		return "envoy-gateway"
+	case CertEnvoyRateLimit:
+		return "envoy-rate-limit"
+	case CertEnvoyOidcHmac:
+		return "envoy-oidc-hmac"
+	default:
+		return ""
+	}
+}
+
+func (c CertType) TLSCertFilePath(homeDir string) string {
+	if c == CertEnvoyOidcHmac {
+		return ""
+	}
+	return c.FilePath(CertificateFilename, homeDir, CertBaseDir)
+}
+
+func (c CertType) TLSKeyFilepath(homeDir string) string {
+	if c == CertEnvoyOidcHmac {
+		return ""
+	}
+	return c.FilePath(PrivateKeyFilename, homeDir, CertBaseDir)
+}
+
+func (c CertType) TLSCaFilePath(homeDir string) string {
+	if c == CertEnvoyOidcHmac {
+		return ""
+	}
+	return c.FilePath(CACertificateFilename, homeDir, CertBaseDir)
+}
+
+func (c CertType) HmacSecretPath(homeDir string) string {
+	if c == CertEnvoyOidcHmac {
+		return c.FilePath(HmacSecretFilename, homeDir, CertBaseDir)
+	}
+	return ""
+
+}
