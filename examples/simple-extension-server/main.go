9ec45fe99b04afe450fb927d8169b448bf9fdeda
Yujie Wu <coffee377@dingtalk.com>
2025-10-13 16:12:53 +0800

    feat: add ext auth metadata and cluster annotations support

--- examples/simple-extension-server/main.go
+++ examples/simple-extension-server/main.go
@@ -15,10 +15,17 @@ import (
 	"strings"
 	"syscall"
 
+	corev3 "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
+	listenerv3 "github.com/envoyproxy/go-control-plane/envoy/config/listener/v3"
+	extauthv3 "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/http/ext_authz/v3"
+	hcm "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/network/http_connection_manager/v3"
+	"github.com/envoyproxy/go-control-plane/pkg/wellknown"
 	"github.com/urfave/cli/v2"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/anypb"
+	"google.golang.org/protobuf/types/known/structpb"
 
 	pb "github.com/envoyproxy/gateway/proto/extension"
 )
@@ -139,14 +146,110 @@ func (s *Server) PostVirtualHostModify(ctx context.Context, req *pb.PostVirtualH
 func (s *Server) PostHTTPListenerModify(ctx context.Context, req *pb.PostHTTPListenerModifyRequest) (*pb.PostHTTPListenerModifyResponse, error) {
 	s.log.Info("postHTTPListenerModify callback was invoked")
 
+	filterChains := req.Listener.GetFilterChains()
+	defaultFC := req.Listener.DefaultFilterChain
+	if defaultFC != nil {
+		filterChains = append(filterChains, defaultFC)
+	}
+	for _, currChain := range filterChains {
+		httpConManager, hcmIndex, err := findHCM(currChain)
+		if err != nil {
+			s.log.Error("failed to find an HCM in the current chain", slog.Any("error", err))
+			continue
+		}
+		// If a basic ext auth filter already exists, update it.
+		extAuth, extIndex, err := findExtAuthFilter(httpConManager.HttpFilters)
+		if extIndex != -1 {
+			switch svc := extAuth.Services.(type) {
+			case *extauthv3.ExtAuthz_GrpcService:
+				// svc 是 *ext_authz_v3.ExtAuthz_GrpcService 类型
+				// 我们可以安全地访问它的 GrpcService 字段
+				grpcService := svc.GrpcService
+				if grpcService.InitialMetadata == nil {
+					grpcService.InitialMetadata = make([]*corev3.HeaderValue, 0)
+				}
+				grpcService.InitialMetadata = append(grpcService.InitialMetadata,
+					&corev3.HeaderValue{Key: "x-target-cluster", Value: "%UPSTREAM_CLUSTER%"},
+					&corev3.HeaderValue{Key: "x-target-service", Value: "%CLUSTER_METADATA(annotations:service)%"},
+				)
+			// Update the ext auth filter
+			case *extauthv3.ExtAuthz_HttpService:
+
+			}
+		}
+		s.log.Info("extAuth filter not found, creating a new one", extAuth)
+
+		anyEAFilter, _ := anypb.New(extAuth)
+		if extIndex > -1 {
+			httpConManager.HttpFilters[extIndex].ConfigType = &hcm.HttpFilter_TypedConfig{
+				TypedConfig: anyEAFilter,
+			}
+		}
+		// Write the updated HCM back to the filter chain
+		anyConnectionMgr, _ := anypb.New(httpConManager)
+		currChain.Filters[hcmIndex].ConfigType = &listenerv3.Filter_TypedConfig{
+			TypedConfig: anyConnectionMgr,
+		}
+	}
+
 	return &pb.PostHTTPListenerModifyResponse{
 		Listener: req.Listener,
 	}, nil
 }
 
+func findExtAuthFilter(filters []*hcm.HttpFilter) (*extauthv3.ExtAuthz, int, error) {
+	for i, filter := range filters {
+		if strings.HasPrefix(filter.Name, "envoy.filters.http.ext_authz") {
+			extAuthz := new(extauthv3.ExtAuthz)
+			if err := filter.GetTypedConfig().UnmarshalTo(extAuthz); err != nil {
+				return nil, -1, err
+			}
+			return extAuthz, i, nil
+		}
+	}
+	return nil, -1, nil
+}
+
+// Tries to find an HTTP connection manager in the provided filter chain.
+func findHCM(filterChain *listenerv3.FilterChain) (*hcm.HttpConnectionManager, int, error) {
+	for filterIndex, filter := range filterChain.Filters {
+		if filter.Name == wellknown.HTTPConnectionManager {
+			hcm := new(hcm.HttpConnectionManager)
+			if err := filter.GetTypedConfig().UnmarshalTo(hcm); err != nil {
+				return nil, -1, err
+			}
+			return hcm, filterIndex, nil
+		}
+	}
+	return nil, -1, fmt.Errorf("unable to find HTTPConnectionManager in FilterChain: %s", filterChain.Name)
+}
+
 func (s *Server) PostTranslateModify(ctx context.Context, req *pb.PostTranslateModifyRequest) (*pb.PostTranslateModifyResponse, error) {
 	s.log.Info("PostTranslateModify callback was invoked")
 
+	for _, cluster := range req.GetClusters() {
+		if strings.Contains(cluster.Name, "basic-api") {
+			service := &structpb.Value{
+				Kind: &structpb.Value_StructValue{StructValue: &structpb.Struct{
+					Fields: map[string]*structpb.Value{
+						"id":          {Kind: &structpb.Value_StringValue{StringValue: "0001"}},
+						"name":        {Kind: &structpb.Value_StringValue{StringValue: "basic-api"}},
+						"version":     {Kind: &structpb.Value_StringValue{StringValue: "v1.0.0"}},
+						"environment": {Kind: &structpb.Value_StringValue{StringValue: "dev"}},
+					},
+				}},
+			}
+			m := map[string]*structpb.Struct{}
+			m["annotations"] = &structpb.Struct{
+				Fields: map[string]*structpb.Value{
+					"service": service,
+				},
+			}
+
+			cluster.Metadata.FilterMetadata = m
+
+		}
+	}
 	return &pb.PostTranslateModifyResponse{
 		Secrets:  req.Secrets,
 		Clusters: req.Clusters,
